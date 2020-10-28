546de82829a90a8f0040819355aa008b656f7e89
yamag2 <y.yamaguchi.bz@gmail.com>
2020-10-28 23:18:56 +0900

    sda

--- src/.vuepress/components/NetlifyForm.vue
+++ src/.vuepress/components/NetlifyForm.vue
@@ -1,20 +1,22 @@
 <template>
-<form name="contact" method="POST"
-  data-netlify-honeypot="bot-field"
-  data-netlify-recaptcha="true"
-  data-netlify="true">
-  <fieldset id="sign_up">
-    <legend>CONTACT US!</legend>
-    <div>
-      <label for="fullname">Ur Name</label>
-      <input type="text" name="fullname" id="fullname"
-       placeholder="Input Ur Name">
-    </div>
-    <input type="hidden" name="bot-field" />
-  </fieldset>
-  <div data-netlify-recaptcha="true"></div>
-  <div >
-    <input type="submit" value="Send">
-  </div>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
 </form>
 </template>
